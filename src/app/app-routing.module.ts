88a8f500baff86429f9866fca48216969dbaafe9
Nicolaspdle96 <nicolaspdle@gmail.com>
2017-09-25 22:19:26 -0400

    Commit inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DemoComponent } from "./components/demo/demo.component";
+import { Comp1Component } from "./components/comp1/comp1.component";
+import { Comp2Component } from "./components/comp2/comp2.component";
+import { Comp3Component } from "./components/comp3/comp3.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: 'demo', component: DemoComponent } ,
+ { path: 'comp1', component: Comp1Component } ,
+ { path: 'comp2', component: Comp2Component } ,
+ { path: 'comp3', component: Comp3Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar.component';
+import { DemoComponent } from './components/demo/demo.component';
+import { Comp1Component } from './components/comp1/comp1.component';
+import { Comp2Component } from './components/comp2/comp2.component';
+import { Comp3Component } from './components/comp3/comp3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DemoComponent,
+    Comp1Component,
+    Comp2Component,
+    Comp3Component
   ],
   imports: [
     BrowserModule,
